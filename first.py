f6ddc02abeb2136a99319892b0eb5b1f1444bf00
Surekha Kolluri <kolluri@Narendras-MacBook-Pro.local>
2018-12-28 18:47:26 -0600

    first commit

---
+++ first.py
@@ -0,0 +1,12 @@
+python_course = True
+java_course = False
+int(python_course) == 1
+int(java_course) == 0
+str(python_course) == "True"
+aliens_found = None
+
+number = 5
+if number:
+    print("number is 5")
+else:
+    print("number is not 5")
