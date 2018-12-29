169affcdb254f5e93ff4ac07d6a57399c64723d8
Surekha Kolluri <kolluri@Narendras-MacBook-Pro.local>
2018-12-28 18:53:25 -0600

    added print statements

--- first.py
+++ first.py
@@ -5,6 +5,9 @@ int(java_course) == 0
 str(python_course) == "True"
 
 number = 5
+print(f"index is {number}")
+print("another index is {0}+{1}={2}".format(number, number, (number + number)))
+
 if number:
     print("number is 5")
 else:
